3d17513592d783d66bc13c3b3306e3e17c9dd1ab
Abhijeet Singh Parihar <abhibuisness123@gmail.com>
2025-07-11 15:16:19 +0530

    fix

--- app/Orders/page.tsx
+++ app/Orders/page.tsx
@@ -5,7 +5,6 @@ import PageTitle from '@/components/PageTitle'
 import { cn } from '@/lib/utils'
 import { ColumnDef } from '@tanstack/react-table'
 import React from 'react'
-import { data } from '../../lib/data/orders-data'
 
 type Props = {}
 export type Payment = {
@@ -14,6 +13,99 @@ export type Payment = {
   lastorder: string,
   method: string
 }
+
+export const data: Payment[] = [
+  {
+    order: "ORD001",
+    status: "Pending",
+    lastorder: "2024-06-15",
+    method:"Credit Card"
+  },
+  {
+    order: "ORD002",
+    status: "Processing",
+    lastorder: "2024-07-01",
+    method:"Paypal"
+  },
+  {
+    order: "ORD003",
+    status: "Pending",
+    lastorder: "2024-06-24",
+    method:"Credit Card"
+  },
+  {
+    order: "ORD004",
+    status: "Completed",
+    lastorder: "2024-06-05",
+    method:"Credit Card"
+  },
+  {
+    order: "ORD005",
+    status: "Processing",
+    lastorder: "2024-07-15",
+    method:"Debit Card"
+  },
+  {
+    order: "ORD006",
+    status: "Completed",
+    lastorder: "2024-06-15",
+    method:"Credit Card"
+  },
+  {
+    order: "ORD007",
+    status: "Pending",
+    lastorder: "2024-07-01",
+    method:"Paypal"
+  },
+  {
+    order: "ORD008",
+    status: "Pending",
+    lastorder: "2024-06-24",
+    method:"Credit Card"
+  },
+  {
+    order: "ORD009",
+    status: "Completed",
+    lastorder: "2024-06-05",
+    method:"Credit Card"
+  },
+  {
+    order: "ORD0010",
+    status: "Processing",
+    lastorder: "2024-07-15",
+    method:"Debit Card"
+  },
+  {
+    order: "ORD0011",
+    status: "Processing",
+    lastorder: "2024-06-15",
+    method:"Credit Card"
+  },
+  {
+    order: "ORD0012",
+    status: "Completed",
+    lastorder: "2024-07-01",
+    method:"Paypal"
+  },
+  {
+    order: "ORD0013",
+    status: "Pending",
+    lastorder: "2024-06-24",
+    method:"Credit Card"
+  },
+  {
+    order: "ORD0014",
+    status: "Completed",
+    lastorder: "2024-06-05",
+    method:"Credit Card"
+  },
+  {
+    order: "ORD0015",
+    status: "Processing",
+    lastorder: "2024-07-15",
+    method:"Debit Card"
+  },
+]
 
 export const columns: ColumnDef<Payment>[] = [
   {
